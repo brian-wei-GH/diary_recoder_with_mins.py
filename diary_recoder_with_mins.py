3aa08d6b964429609499e62ff69905ef62e4370e
brian-wei-GH <wwh2188@gmail.com>
2024-07-01 00:45:25 -0700

    Add files via upload

---
+++ diary_recoder_with_mins.py
@@ -0,0 +1,15 @@
+import datetime
+
+
+def run():
+    while True:
+        text = input("Enter your text: ")
+        if text.upper() == "Q":
+            break
+        date = datetime.datetime.now().strftime("%Y-%m-%d, %H:%M")
+        text = "{}\n".format(text)
+        with open(date, "a") as f:
+            f.write(text)
+
+
+run()
